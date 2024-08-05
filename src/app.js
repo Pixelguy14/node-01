fbb6d3c89743a20edfe57660aae2f1701df9d7c4
Pixelguy14 <pixelguy14@gmail.comgit config --global user.name Pixelguy14git config --global user.email pixelguy14@gmail.com>
2024-08-05 09:20:31 -0600

    agregamos src y archivos

---
+++ src/app.js
@@ -0,0 +1,5 @@
+const frutas = require('./frutas')
+
+frutas.forEach((fruta,index)=>{
+    console.log("fruta: ",fruta, index)
+})
---
+++ src/frutas.js
@@ -0,0 +1,7 @@
+const frutas = ['limon','naranja','sandia','toronja','manzana']
+/*
+frutas.forEach((fruta)=>{
+    console.log("fruta: ",fruta)
+})
+*/
+module.exports = frutas
